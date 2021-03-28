a54ba423daef380fe7903b883fc1de258775a306
Tobias <tobi.braendle@gmail.com>
2021-03-28 12:56:24 +0900

    css darkmode

--- _stylelink.php
+++ _stylelink.php
@@ -3,4 +3,4 @@
 <link rel="stylesheet" href="CSS/reset.css">
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@700&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="CSS/style2.css">
+<link rel="stylesheet" href="CSS/style3.css">
